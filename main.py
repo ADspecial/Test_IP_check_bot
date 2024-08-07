bbaea96bed133c716d4a723857df4bcca3eee9c5
ADspecial <lekontsev.danil@gmail.com>
2024-08-07 10:26:04 +0700

    Структура бота

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+#Точка входа
+
 import sys;
 from includes.settings import *;
-import telebot;
 from includes.single_ip import ip_info
+from includes.ip_list import process_ip_list
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 import vt;
 
 
@@ -14,9 +20,26 @@ def ensure_directories():
 #print(API_URL_IP_VT)
 #print(VT_KEY)
 
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=TG_KEY)
+# Диспетчер
+dp = Dispatcher()
 
-test = ip_info('193.124.92.156')
-print(test)
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
 '''
 bot = telebot.TeleBot(TG_KEY);
 
@@ -30,4 +53,10 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help")
 
 bot.polling(none_stop=True, interval=0)
+
+test = ip_info('193.124.92.111')
+print(test)
+
+file_path = input("Type the input file (Example: input.txt): ")
+process_ip_list(file_path)
 '''
