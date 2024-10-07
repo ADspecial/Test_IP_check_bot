fef678f306b2f3235d7ad01f0c337fdb8640ea39
ADspecial <lekontsev.danil@gmail.com>
2024-10-07 17:10:22 +0700

    07.10.2024 change kaspersky.py add gen_result

--- ipcheckers/kaspersky.py
+++ ipcheckers/kaspersky.py
@@ -1,26 +1,23 @@
 import json
-import requests
-from ipaddress import ip_address
-from ipcheckers.geo_ip import get_geo_response
-from config.config import KEYS
-from ipcheckers.valid_ip import extract_and_validate
-
 import aiohttp
-from aiohttp import ClientSession
 import asyncio
+import requests
+
+from config.config import KEYS
+from ipcheckers.format import get_country_flag
+
 from typing import List, Dict, Union, Tuple
 from datetime import datetime
 
+
 from config.config import KEYS, URLS
-from ipcheckers.format import get_country_flag
 
-
-async def make_request_kaspersky(ip: str, session: ClientSession):
+async def make_request_kaspersky(ip: str):
     headers = {
         'x-api-key': KEYS.KASPERSKY_KEY
     }
-    async with session.get(URLS.API_URL_KASPERSKY+"?request="+ip, headers=headers) as response:
-        return json.loads(await response.text()) if response.status == 200 else None
+    response = requests.request(method='GET', url=URLS.API_URL_KASPERSKY + "?request=" + ip, headers=headers).json()
+    return gen_result(response)
 
 
 async def get_kaspersky_info(
@@ -35,13 +32,13 @@ async def get_kaspersky_info(
     Returns:
         A tuple containing a boolean indicating success and a list of dictionaries containing the response data.
     """
-    async with aiohttp.ClientSession() as session:
-        tasks = [make_request_kaspersky(ip, session) for ip in ips]
-        results = await asyncio.gather(*tasks, return_exceptions=True)
-        filtered_results = [
-            result for result in results if not isinstance(result, Exception)
-        ]
-        return True, filtered_results
+    results = []
+    tasks = [make_request_kaspersky(ip) for ip in ips]
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+    filtered_results = [
+        result for result in results if not isinstance(result, Exception)
+    ]
+    return True, filtered_results
 
 
 def gen_result(response: Dict[str, Union[str, int]]) -> Dict[str, Union[str, int, datetime]]:
@@ -55,17 +52,11 @@ def gen_result(response: Dict[str, Union[str, int]]) -> Dict[str, Union[str, int
         A dictionary with the response data.
     """
     result = {
-        "ip_address": response["ipAddress"],
-        "is_public": response["isPublic"],
-        "ip_version": response["ipVersion"],
-        "is_whitelisted": response["isWhitelisted"],
-        "abuse_confidence_score": response["abuseConfidenceScore"],
-        "country": get_country_flag(response["countryCode"]),
-        "usage_type": response["usageType"],
-        "isp": response["isp"],
-        "domain": response["domain"],
-        "total_reports": response["totalReports"],
-        "num_distinct_users": response["numDistinctUsers"],
-        #"last_reported_at":response.get("lastReportedAt")
+        'ip_address': response['IpGeneralInfo']['Ip'],
+        'status': response['IpGeneralInfo']['Status'],
+        'country': get_country_flag(response['IpGeneralInfo']['CountryCode']),
+        'net_name': response["IpWhoIs"]["Net"]["Name"],
+        'zone': response['Zone'],
+        'last_changed_at': response["IpWhoIs"]['Net']['Changed'],
     }
     return result
